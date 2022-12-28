bf56d97af3654ddcae64bf5dea6db26be8718622
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2022-12-28 18:24:05 +0900

    [Silver II] Title: 좌표 압축, Time: 1844 ms, Memory: 153964 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -3,8 +3,7 @@ input = sys.stdin.readline
 
 n = int(input())
 ls = list(map(int,input().split()))
-ls2 = list(set(ls))
-ls2.sort()
+ls2 = sorted(list(set(ls)))
 ls3 = {ls2[i]: i for i in range(len(ls2))}
 for i in ls:
     print(ls3[i],end=' ')
