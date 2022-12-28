054be27b726b416487f52749b094326a2408f4c4
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2022-12-28 13:53:57 +0900

    [Silver V] Title: 단어 정렬, Time: 72 ms, Memory: 35272 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -5,9 +5,9 @@ n = int(input())
 ls = []
 for _ in range(n):
     word = input().rstrip()
-    if word not in ls:
-        ls.append(word)
+    ls.append(word)
+ls = list(set(ls))
 ls.sort()
 ls.sort(key=len)
 for word in ls:
-    print(word, sep='\n')
+    print(word)
