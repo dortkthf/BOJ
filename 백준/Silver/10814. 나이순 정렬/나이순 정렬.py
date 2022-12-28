151768d30181105955631664035afa3a77fb7089
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2022-12-28 16:09:02 +0900

    [Silver V] Title: 나이순 정렬, Time: 292 ms, Memory: 50256 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -3,11 +3,9 @@ input = sys.stdin.readline
 
 n = int(input())
 ls = []
-cnt=0
 for _ in range(n):
     age, name = input().split()
-    ls.append([int(age),cnt,name])
-    cnt+=1
+    ls.append([int(age),_,name])
 ls.sort()
 for l,s,i in ls:
     print(l,i)
