517935ce77a5ae06ccc0adadc3f10c25343d6df4
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-02 23:04:03 +0900

    [Silver III] Title: 문자열 집합, Time: 124 ms, Memory: 42348 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,6 +1,9 @@
+import sys
+input = sys.stdin.readline
+
 n,m = map(int,input().split())
-S = set(input() for i in range(n))
-N = list(input() for i in range(m))
+S = set(input().rstrip() for i in range(n))
+N = list(input().rstrip() for i in range(m))
 cnt = 0
 for i in N:
     if i in S:
