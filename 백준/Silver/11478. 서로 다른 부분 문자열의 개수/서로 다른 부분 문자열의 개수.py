e7089c595da6865d02d7a6ce06aaea3140437b73
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-02 23:59:34 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 584 ms, Memory: 240672 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,13 +1,13 @@
 s = input()
 cnt = 1
-res = []
+res = set()
 while True:
     for i in range(len(s)):
         if i+cnt <= len(s):
-            res.append(s[0+i:cnt+i])
+            res.add(s[0+i:cnt+i])
         else:
             break
     cnt+=1
     if cnt>len(s):
         break
-print(len(set(res)))
+print(len(res))
