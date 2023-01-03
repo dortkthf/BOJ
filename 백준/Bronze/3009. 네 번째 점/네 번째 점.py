f45c4eb24471863b94b5b9381cdb02164143d923
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-03 22:26:34 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,24 @@
+dic1 = {}
+dic2 = {}
+for i in range(3):
+    x,y = map(int,input().split())
+    if x in dic1:
+        dic1[x] += 1
+    else:
+        dic1[x] = 1
+    
+    if y in dic2:
+        dic2[y] += 1
+    else:
+        dic2[y] = 1
+
+res = []
+for k,v in dic1.items():
+    if v == 1:
+        res.append(k)
+
+for k,v in dic2.items():
+    if v == 1:
+        res.append(k)
+
+print(*res, sep=' ')
