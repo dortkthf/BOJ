d00fc8277a095d400b52695925713069e3a74fae
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-05 17:46:57 +0900

    [Bronze I] Title: 최소공배수, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,27 +1,8 @@
 import sys, math
 input = sys.stdin.readline
-res = []
 t = int(input())
+res = []
 for _ in range(t):
-    a,b = map(int,input().split())
-    al = []
-    for i in range(1,int(math.sqrt(a))+1):
-        if a%i == 0:
-            al.append(i)
-            al.append(a//i)
-    al.sort(reverse=True)
-
-    
-    bl = []
-    for i in range(1,int(math.sqrt(b))+1):
-        if b%i == 0:
-            bl.append(i)
-            bl.append(b//i)
-    bl.sort(reverse=True)
-
-    for i in al:
-        if i in bl:
-            gcd = i
-            break
-    res.append((int(a*b)//gcd))
+    x,y = map(int,input().split())
+    res.append(math.lcm(x,y))
 print(*res, sep='\n')
