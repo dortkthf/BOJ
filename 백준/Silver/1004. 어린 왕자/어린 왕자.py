1f6e2b5b26193a746be2b1264f34bb8c34c9c9fe
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-05 16:35:40 +0900

    [Silver III] Title: 어린 왕자, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -8,8 +8,8 @@ for _ in range(t):
     n = int(input())
     for _ in range(n):
         x,y,r = map(int,input().split())
-        if ((x-x1)**2+(y-y1)**2) < r**2 and ((x-x2)**2+(y-y2)**2) < r**2:
-            pass
-        elif (((x-x1)**2+(y-y1)**2) < r**2 and ((x-x2)**2+(y-y2)**2) > r**2) or (((x-x1)**2+(y-y1)**2) > r**2 and ((x-x2)**2+(y-y2)**2) < r**2):
+        res1 = ((x-x1)**2+(y-y1)**2) - r**2
+        res2 = ((x-x2)**2+(y-y2)**2) - r**2
+        if res1*res2 < 0:
             cnt+=1
     print(cnt)
