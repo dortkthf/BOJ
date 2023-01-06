abcb69eaaf9318cf17105461f112cd44d6c95233
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-06 17:42:56 +0900

    [Silver IV] Title: 링, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Silver/3036. 링/링.py
+++ 백준/Silver/3036. 링/링.py
@@ -1,7 +1,7 @@
-from fractions import Fraction
+import math
 
 n = int(input())
 ring = list(map(int,input().split()))
 for i in range(1,n):
-    a = Fraction(ring[0],ring[i])
-    print(a.numerator,a.denominator,sep='/')
+    gcd = math.gcd(ring[0],ring[i])
+    print(ring[0]//gcd,ring[i]//gcd,sep='/')
