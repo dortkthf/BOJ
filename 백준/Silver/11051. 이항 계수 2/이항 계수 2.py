6cfc22206ddc2f19533b902b181f529613da165d
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-06 19:21:04 +0900

    [Silver II] Title: 이항 계수 2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,15 +1,13 @@
 n,k = map(int,input().split())
 
-nn = 1
-kk = 1
-nk = 1
-for i in range(2,n+1):
-    nn*=i
-for i in range(2,k+1):
-    kk*=i
-for i in range(2,n-k+1):
-    nk*=i
 
-ans = nn//kk//nk
+def fac(a):
+    res = 1
+    for i in range(2,a+1):
+        res*=i
+    return res
+
+ans = fac(n)//fac(k)//fac(n-k)
+
 
 print(round(ans%10007))
