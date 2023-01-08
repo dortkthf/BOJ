7ec7194a3ad7a07ed3a6ca1dead9ec9542388509
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-08 17:37:14 +0900

    [Silver III] Title: N과 M (3), Time: 1896 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,15 @@
+n,m = map(int,input().split())
+
+nums = list(i for i in range(1,n+1))
+
+s = []
+def sol():
+    for i in nums:
+        s.append(i)
+        if len(s) == m:
+            print(*s)
+            s.pop()
+            continue
+        sol()
+        s.pop()
+sol()
