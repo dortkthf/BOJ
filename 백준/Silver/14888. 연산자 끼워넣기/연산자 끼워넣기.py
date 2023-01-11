0385378c5795aa487da7ebd1eb3d6fde938b0301
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-11 18:38:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 612 ms, Memory: 36920 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,42 @@
+# itertools 의 permutations를 사용하여 문제를 해결하기
+
+from itertools import permutations
+
+n = int(input())
+nums = list(map(int,input().split()))
+
+opers = list(map(int,input().split()))
+opers2 = ['+','-','x','/']
+
+operators = []
+
+for i in range(len(opers)):
+    for j in range(opers[i]):
+        operators.append(opers2[i])
+
+operlist = permutations(operators,len(operators))
+operlist = set(operlist)
+
+res = []
+
+for i in operlist:
+    for j in range(n):
+        if j == 0:
+            fst = nums[0]
+            continue
+        elif i[j-1] == '+':
+            fst+=nums[j]
+        elif i[j-1] == '-':
+            fst-=nums[j]
+        elif i[j-1] == 'x':
+            fst*=nums[j]
+        elif i[j-1] == '/':
+            if fst<0:
+                fst = -fst
+                fst//=nums[j]
+                fst = -fst
+            else:
+                fst//=nums[j]
+    res.append(fst)
+
+print(max(res),min(res),sep='\n')
