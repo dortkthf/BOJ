2107c1ee41220c10a1602e1db9fb51f150c5751f
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-24 18:13:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 3744 ms, Memory: 34312 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -4,18 +4,10 @@ input = sys.stdin.readline
 n,k = map(int,input().split())
 dp = [0 for i in range(k+1)]
 
-ws = [0]
-vs = [0]
-
 for i in range(n):
     w,v = map(int,input().split())
-    ws.append(w)
-    vs.append(v)
-
-for i in range(1,n+1):
     for j in range(k,0,-1):
-        if ws[i] <= j:
-            dp[j] = max(dp[j], dp[j-ws[i]]+vs[i])
-        continue
+        if w<=j:
+            dp[j] = max(dp[j], dp[j-w]+v)
 
 print(dp[-1])
