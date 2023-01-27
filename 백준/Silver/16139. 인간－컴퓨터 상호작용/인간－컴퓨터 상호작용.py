c75259423df95601b256b993205abd20f24a89eb
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-27 14:38:05 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 228 ms, Memory: 30820 KB -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,26 @@
+words = input()
+wordscnt = len(words)
+
+q = int(input())
+
+dic = {}
+
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    dic[i] = [0 for i in range(wordscnt)]
+
+for i in range(wordscnt):
+    if i == 0:
+        dic[words[i]][i] = 1
+        continue
+    for j in dic:
+        if j == words[i]:
+            dic[j][i] = dic[j][i-1]+1
+            continue
+        dic[j][i] = dic[j][i-1]
+
+for _ in range(q):
+    a,i,r = input().split()
+    if int(i) == 0:
+        print(dic[a][int(r)])
+        continue
+    print(dic[a][int(r)]-dic[a][int(i)-1])
