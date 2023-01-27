2d6a741ed32efea682c64986dcd25074df76831c
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-27 15:50:50 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 468 ms, Memory: 177528 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,29 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-words = input()
-wordscnt = len(words)
-
+s = input().rstrip()
+scnt = len(s)
 q = int(input())
 
-dic = {}
+lst = [list(0 for i in range(26))]
 
-for i in 'abcdefghijklmnopqrstuvwxyz':
-    dic[i] = [0 for i in range(wordscnt)]
+for i in range(scnt):
+    v = lst[i].copy()
+    v[ord(s[i])-97] += 1
+    lst.append(v)
 
-for i in range(wordscnt):
-    if i == 0:
-        dic[words[i]][i] = 1
-        continue
-    for j in dic:
-        if j == words[i]:
-            dic[j][i] = dic[j][i-1]+1
-            continue
-        dic[j][i] = dic[j][i-1]
-
-for _ in range(q):
-    a,i,r = input().split()
-    if int(i) == 0:
-        print(dic[a][int(r)])
-        continue
-    print(dic[a][int(r)]-dic[a][int(i)-1])
+for i in range(q):
+    a,l,r = input().split()
+    print(lst[int(r)+1][ord(a)-97]-lst[int(l)][ord(a)-97])
