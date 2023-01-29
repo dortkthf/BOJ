546caf4b29b9d80a50afde61a1441014b40491b3
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-29 20:47:26 +0900

    [Gold V] Title: 체스판 다시 칠하기 2, Time: 916 ms, Memory: 351172 KB -BaekjoonHub

---
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n,m,k = map(int,input().split())
+chess = [input().rstrip() for i in range(n)]
+dp_black = [list(0 for i in range(m+1)) for i in range(n+1)]
+dp_white = [list(0 for i in range(m+1)) for i in range(n+1)]
+res = []
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if (i+j)%2 == 0:
+            if chess[i-1][j-1] == 'B':
+                dp_black[i][j] = dp_black[i-1][j] + dp_black[i][j-1] - dp_black[i-1][j-1]
+                dp_white[i][j] = dp_white[i-1][j] + dp_white[i][j-1] - dp_white[i-1][j-1] + 1
+            else:
+                dp_black[i][j] = dp_black[i-1][j] + dp_black[i][j-1] - dp_black[i-1][j-1] + 1
+                dp_white[i][j] = dp_white[i-1][j] + dp_white[i][j-1] - dp_white[i-1][j-1]
+        elif (i+j)%2 != 0:
+            if chess[i-1][j-1] == 'W':
+                dp_black[i][j] = dp_black[i-1][j] + dp_black[i][j-1] - dp_black[i-1][j-1]
+                dp_white[i][j] = dp_white[i-1][j] + dp_white[i][j-1] - dp_white[i-1][j-1] + 1
+            else:
+                dp_black[i][j] = dp_black[i-1][j] + dp_black[i][j-1] - dp_black[i-1][j-1] + 1
+                dp_white[i][j] = dp_white[i-1][j] + dp_white[i][j-1] - dp_white[i-1][j-1]
+
+for i in range(k,n+1):
+    for j in range(k,m+1):
+        res.append(dp_black[i][j]-dp_black[i-k][j]-dp_black[i][j-k]+dp_black[i-k][j-k])
+        res.append(dp_white[i][j]-dp_white[i-k][j]-dp_white[i][j-k]+dp_white[i-k][j-k])
+
+print(min(res))
