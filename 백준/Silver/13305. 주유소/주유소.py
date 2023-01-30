cb938f2fa259369f9386e50ebcc3584dd6143843
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-01-30 19:00:19 +0900

    [Silver III] Title: 주유소, Time: 88 ms, Memory: 41740 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -14,29 +14,15 @@ for i in range(len(oil)):
         oil_fst = oil[i]
         cheap.append(i)
 
-cheap_len = len(cheap)
-
-dp = [0 for i in range(n-1)]
-
-for i in range(n-1):
-    if i == 0:
-        dp[i] = way[i]
-        continue
-    dp[i] = way[i] + dp[i-1]
-
-if cheap_len == 0:
-    print(dp[-1]*oil[0])
-elif cheap_len == 1:
-    print(dp[cheap[0]-1]*oil[0]+(dp[-1]-dp[cheap[0]-1])*oil[cheap[0]])
+if len(cheap) == 0:
+    print(sum(way)*oil[0])
 else:
-    s = 0
     ans = 0
-    for i in range(cheap_len):
-        if i == 0:
-            ans+=dp[cheap[i]-1]*oil[0]
-            s = cheap[i]
-        elif i == cheap_len-1:
-            ans+=((dp[cheap[i]-1]-dp[s-1])*oil[s])+((dp[-1]-dp[cheap[i]-1])*oil[cheap[i]])
-        else:
-            ans+=(dp[cheap[i]-1]-dp[s-1])*oil[s]
+    s = 0
+    for i in range(n-1):
+        if i in cheap:
+            s = i
+            ans+=way[i]*oil[s]
+            continue
+        ans+=way[i]*oil[s]
     print(ans)
