75bd17f448edaab1a4f80cb9e07b15dfa44adaf3
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-02-20 18:45:18 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 3052 ms, Memory: 155600 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,15 +1,30 @@
-import sys, bisect
+import sys
 input = sys.stdin.readline
 
 n = int(input())
 a = list(map(int,input().split()))
-res = [a[0]]
+dp = [0,a[0]] + list(0 for i in range(n-1))
+current = 1
 
-for num in a:
-    if res[-1] < num:
-        res.append(num)
-        continue
-    index = bisect.bisect_left(res,num)
-    res[index] = num
+def bs(left,right,value):
+    mid = (left+right)//2
 
-print(len(res))
+    if value > dp[right]:
+        return -1
+    if right-left == 1:
+        if dp[left] < value and value <= dp[right]:
+            return right
+    if value > dp[mid]:
+        return bs(mid,right,value)
+    if value <= dp[mid]:
+        return bs(left,mid,value)
+
+for i in range(1,n):
+    index = bs(0,current,a[i])
+    if index == -1:
+        current+=1
+        dp[current] = a[i]
+    else:
+        dp[index] = a[i]
+
+print(current)
