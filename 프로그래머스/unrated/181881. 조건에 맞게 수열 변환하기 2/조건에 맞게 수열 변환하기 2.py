b3650990723b52ce0b90fd8e624f8e23f703c62c
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-09 16:22:24 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,15 @@
+import copy
+
+def solution(arr):
+    answer = 0
+    while True:
+        fst = copy.deepcopy(arr)
+        for i in range(len(arr)):
+            if arr[i]>=50 and arr[i]%2 == 0:
+                arr[i] = arr[i]//2
+            elif arr[i]<50 and arr[i]%2 == 1:
+                arr[i] = (arr[i]*2)+1
+        if fst == arr:
+            return answer
+        answer+=1
+    return answer
