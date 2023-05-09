b37e24547ec2e0a5c009154001d96132adfaf5f4
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-09 18:55:29 +0900

    [unrated] Title: 덧칠하기, Time: 3.18 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -4,9 +4,7 @@ def solution(n, m, section):
         if i == 0:
             s = section[i] + m-1
             answer+=1            
-        elif s>=section[i]:
-            continue
-        else:
+        elif s<section[i]:
             s = section[i]+ m-1
             answer+=1
     return answer
