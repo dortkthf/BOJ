3ac6503fe853bc2489ab3453b4009963c721b2c5
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-10 20:59:55 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.50 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,15 +1,9 @@
+def cf(n): # 공약수 출력
+    a = []
+    for i in range(1,int(n**0.5)+1):
+        if n%i == 0:
+            a.append(n//i)
+            a.append(i)
+    return len(set(a))
 def solution(number, limit, power):
-    answer = 0
-    res = []
-    for i in range(1,number+1):
-        tmp = 0
-        for j in range(1,int(i**0.5)+1):
-            if i % j == 0:
-                if i//j == j:
-                    tmp+=1
-                else:
-                    tmp+=2
-        if tmp > limit:
-            tmp = power
-        res.append(tmp)
-    return sum(res)    
+    return sum([cf(i) if cf(i)<=limit else power for i in range(1,number+1)])
