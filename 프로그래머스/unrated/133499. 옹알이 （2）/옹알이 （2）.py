35e288f2992baafd69da565f1bdbbd99a2adb7db
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-11 16:54:03 +0900

    [unrated] Title: 옹알이 (2), Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,15 +1,11 @@
 def solution(babbling):
     answer = 0
-    for i in range(len(babbling)):
-        babbling[i] = babbling[i].replace('aya','1')
-        babbling[i] = babbling[i].replace('ye','2')
-        babbling[i] = babbling[i].replace('woo','3')
-        babbling[i] = babbling[i].replace('ma','4')
-    
-    for bab in babbling:
-        if bab.isdigit():
-            if '11' in bab or '22' in bab or '33' in bab or '44' in bab:
-                continue
+    for b in babbling:
+        for j in ["aya", "ye", "woo", "ma"]:
+            if j*2 in b:
+                break
             else:
-                answer+=1
+                b = b.replace(j,' ')
+        if len(b.replace(' ','')) == 0:
+            answer+=1
     return answer
