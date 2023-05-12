9e31627d046e04701d1ce3e148713c5d7ac18055
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-12 16:40:59 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,30 @@
+def solution(new_id):
+    answer = ''
+    new_id = new_id.lower() 
+    for i in new_id:
+        if i.isalpha() or i.isdigit() or i in ['-','_','.']:
+            continue
+        else:
+            new_id = new_id.replace(i,'')
+    while '..' in new_id:
+        new_id = new_id.replace('..','.')
+    if len(new_id) > 0 and new_id[0] == '.':
+        if len(new_id) == 1:
+            new_id = ''
+        else:
+            new_id = new_id[1:]
+    if len(new_id) > 0 and new_id[-1] == '.':
+        if len(new_id) == 1:
+            new_id = ''
+        else:
+            new_id = new_id[:-1]
+    if new_id == '':
+        new_id = 'a'
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
+    if len(new_id) <= 2:
+        while len(new_id) < 3:
+            new_id += new_id[-1]
+    return new_id
