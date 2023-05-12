fa6bde6e92dd7c2eacae0ceba158cc26ff65bb8c
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-12 21:12:21 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,25 +1,11 @@
 def solution(N, stages):
-    answer = []
-    stay = [0 for i in range(N+2)]
-    
-    for i in stages:
-        stay[i] += 1
-    
-    accumulation = stay[::-1]
-    tmp = 0
-    
-    for i in range(len(accumulation)):
-        if accumulation[i] != 0:
-            accumulation[i]+=tmp
-            tmp=accumulation[i]
-    
-    accumulation = accumulation[::-1]
-    
-    for i in range(1,N+1):
-        if stay[i] != 0:
-            answer.append((stay[i]/accumulation[i],i))
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
         else:
-            answer.append((0,i))
-    answer.sort(key = lambda x: (-x[0],x[1]))
-    res = [i[1] for i in answer ]
-    return res
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
