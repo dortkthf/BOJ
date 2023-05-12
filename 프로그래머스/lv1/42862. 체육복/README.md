791086e8f58a4a5243bd403028a7205ec7caeb8c
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-12 22:36:53 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/README.md
+++ 프로그래머스/lv1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,14 +1,11 @@
 def solution(n, lost, reserve):
     answer = 0
-    nlost = list(set(lost[:])-set(reserve[:]))
-    nreserve = set(reserve[:])-set(lost[:])
-    lost = nlost[:]
-    lost.sort()
-    for i in lost:
-        if i-1 in nreserve:
-            nreserve.remove(i-1)
-            nlost.remove(i)
-        elif i+1 in nreserve:
-            nreserve.remove(i+1)
-            nlost.remove(i)
+    nlost = list(set(lost)-set(reserve))
+    nreserve = set(reserve)-set(lost)
+    nlost.sort()
+    for i in nreserve:
+        if i-1 in nlost:
+            nlost.remove(i-1)
+        elif i+1 in nlost:
+            nlost.remove(i+1)
     return n-len(nlost)
