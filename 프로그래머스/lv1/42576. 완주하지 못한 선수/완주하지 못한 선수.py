f23ebce2a0ca442a1b1586b9da080a6564f2e5c5
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-13 10:34:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.15 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,6 @@
+import collections
+
+
 def solution(participant, completion):
-    answer = ''
-    marathon = {}
-    
-    for i in participant:
-        if i in marathon:
-            marathon[i][0]+=1
-        else:
-            marathon[i] = [1,0]
-    
-    for i in completion:
-        marathon[i][1] +=1
-    
-    for k,v in marathon.items():
-        if v[0] != v[1]:
-            return k
-    
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
