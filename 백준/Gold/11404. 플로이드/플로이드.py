dff7b2b9442c30205cb0e898a3f84b6aa78375d2
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-15 20:20:53 +0900

    [Gold IV] Title: 플로이드, Time: 4952 ms, Memory: 43964 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,32 +1,58 @@
-import collections, sys
-input = sys.stdin.readline
+import sys
+from heapq import heappop, heappush
 
+INF = int(1e9)
+
+# 도시의 개수 n과 버스의 개수 m 입력
 n = int(input())
 m = int(input())
 
-graph = [[] for i in range(n+1)]
+# 그래프 초기화
+graph = [[] for _ in range(n + 1)]
 
+# 버스 정보 입력
 for _ in range(m):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
-def min_price(number):
-    queue = collections.deque()
-    queue.append((number,0))
-    price_list = list(sys.maxsize for _ in range(n+1))
-    price_list[number] = 0
+# 시작 도시에서 각 도시까지의 최소 비용을 저장할 리스트
+cost = [[INF] * (n + 1) for _ in range(n + 1)]
 
-    while queue:
-        start,sprice = queue.popleft()
-        for i in graph[start]:
-            end,eprice = i
-            if price_list[start] != sys.maxsize and price_list[end] > sprice+eprice:
-                price_list[end] = sprice+eprice
-                queue.append((end,sprice+eprice))
-    for i in range(1,n+1):
-        if price_list[i] == sys.maxsize:
-            price_list[i] = 0
-    return price_list[1:]
+# 다익스트라 알고리즘 함수
+def dijkstra(start):
+    # 시작 도시의 최소 비용은 0으로 설정
+    cost[start][start] = 0
 
-for i in range(1,n+1):
-    print(*min_price(i))
+    # 우선순위 큐를 사용한 다익스트라 알고리즘
+    heap = []
+    heappush(heap, (0, start))
+
+    while heap:
+        dist, node = heappop(heap)
+
+        # 이미 처리된 노드는 스킵
+        if cost[start][node] < dist:
+            continue
+
+        # 인접한 도시들을 탐색
+        for next_node, next_dist in graph[node]:
+            new_cost = dist + next_dist
+
+            # 현재까지의 최소 비용보다 더 작은 경우 갱신
+            if new_cost < cost[start][next_node]:
+                cost[start][next_node] = new_cost
+                heappush(heap, (new_cost, next_node))
+
+# 다익스트라 알고리즘을 모든 노드에 대해 실행
+for i in range(1, n + 1):
+    dijkstra(i)
+
+# 결과 출력
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        # 도시 i에서 j로 가는 경로가 없는 경우 0 출력
+        if cost[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(cost[i][j], end=' ')
+    print()
