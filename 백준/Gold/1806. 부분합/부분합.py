a9407ebae45d622c6ea382fdc732213afdea1e7f
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-16 17:28:25 +0900

    [Gold IV] Title: 부분합, Time: 108 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -4,12 +4,8 @@ input = sys.stdin.readline
 n,s = map(int,input().split())
 numbers = list(map(int,input().split()))
 
-if s in numbers:
-    print(1)
-    exit(0)
-
 def find(start,end,res,snum):
-    while start < end and end < n:
+    while start <= end and end < n:
         if snum >= s:
             if res > end-start+1:
                 res = end-start+1
@@ -19,9 +15,8 @@ def find(start,end,res,snum):
             end+=1
             if end < n:
                 snum+=numbers[end]
+    if res == 100001:
+        res = 0
     return res
 
-if find(0,1,100001,numbers[0]+numbers[1]) == 100001:
-    print(0)
-else:
-    print(find(0,1,100001,numbers[0]+numbers[1]))
+print(find(0,0,100001,numbers[0]))
