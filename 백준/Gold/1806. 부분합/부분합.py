2872dc58c662b5a6e14d31415a7b379658924122
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-16 17:24:36 +0900

    [Gold IV] Title: 부분합, Time: 148 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -3,30 +3,25 @@ input = sys.stdin.readline
 
 n,s = map(int,input().split())
 numbers = list(map(int,input().split()))
-sumnums = [0]
-for i in range(n):
-    if i == 0:
-        sumnums.append(numbers[0])
-        continue
-    sumnums.append(sumnums[i]+numbers[i])
-sumnums.append(0)
 
 if s in numbers:
     print(1)
     exit(0)
 
-def find(start,end,res):
-    while start < end and end <= n:
-        sumnum = sumnums[end]-sumnums[start-1]
-        if sumnum >= s:
+def find(start,end,res,snum):
+    while start < end and end < n:
+        if snum >= s:
             if res > end-start+1:
                 res = end-start+1
+            snum-=numbers[start]
             start+=1
-        elif sumnum < s:
+        elif snum < s:
             end+=1
+            if end < n:
+                snum+=numbers[end]
     return res
 
-if find(1,2,100001) == 100001:
+if find(0,1,100001,numbers[0]+numbers[1]) == 100001:
     print(0)
 else:
-    print(find(1,2,100001))
+    print(find(0,1,100001,numbers[0]+numbers[1]))
