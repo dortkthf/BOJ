3f15430bbc33cd9c9bfc24f134e205b45b361dea
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2023-05-25 15:26:09 +0900

    [Gold I] Title: 냅색문제, Time: 76 ms, Memory: 35640 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.py
+++ 백준/Gold/1450. 냅색문제/냅색문제.py
@@ -7,19 +7,18 @@ nums = list(map(int,input().split()))
 left_nums = nums[:n//2]
 right_nums = nums[n//2:]
 
-def make_nums_sum(nums):
-    nums_sum = []
-    for i in range(len(nums)+1):
-        for com in combinations(nums,i):
-            nums_sum.append(sum(com))
-    return nums_sum
+def make_nums(nums_list):
+    sum_nums_list = []
+    for i in range(len(nums_list)+1):
+        for com in combinations(nums_list,i):
+            sum_nums_list.append(sum(com))
+    return sum_nums_list
 
-def find_answer(left,right,c):
+def cal(left_nums,right_nums,c):
+    left_nums.sort()
     ans = 0
-    left.sort()
-    for i in right:
-        maxvalue = c-i
-        ans+=bisect.bisect_right(left,maxvalue)
+    for i in right_nums:
+        ans+=bisect.bisect_right(left_nums,c-i)
     return ans
 
-print(find_answer(make_nums_sum(left_nums),make_nums_sum(right_nums),c))
+print(cal(make_nums(left_nums),make_nums(right_nums),c))
