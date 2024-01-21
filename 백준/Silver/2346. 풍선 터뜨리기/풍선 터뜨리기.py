90e819eb2fd0d7b50a5144da0c857f011716c793
JunYeop Lee <77156476+dortkthf@users.noreply.github.com>
2024-01-22 00:35:39 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34020 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,18 +1,16 @@
 from collections import deque
 
 n = int(input())
-nums = list(map(int,input().split()))
-bln = deque(list(enumerate(nums)))
-
+bln = deque(enumerate(map(int,input().split())))
 ans = []
 
 for i in range(n):
-    idx,loc = bln[0][0],bln[0][1]
+    idx,loc = bln.popleft()
     ans.append(idx+1)
-    bln.popleft()
+
     if loc > 0:
         bln.rotate(-(loc-1))
     else:
         bln.rotate(-loc)
 
-print(*ans)
+print(' '.join(map(str,ans)))
